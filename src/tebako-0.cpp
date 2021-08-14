ace9de494b9bb9392a425e25b6108a29329f8440
Maxim Samsonov <m.samsonov@computer.org>
2021-08-14 18:58:06 +0300

    Included incbin into regular build

--- src/tebako-0.cpp
+++ src/tebako-0.cpp
@@ -1,21 +1,19 @@
 /**
- * \author     Marcus Holland-Moritz (github@mhxnet.de)
- * \copyright  Copyright (c) Marcus Holland-Moritz
  *
- * This file is part of dwarfs.
+ * Copyright (c) 2021 The Unknown Fathers
+ * This file is a part of tebako 
  *
- * dwarfs is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * dwarfs is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with dwarfs.  If not, see <https://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <array>
@@ -773,5 +771,14 @@ namespace dwarfs {
 } // namespace dwarfs
 
 int main(int argc, char** argv) {
-    return dwarfs::safe_main([&] { return dwarfs::run_dwarfs(argc, argv); });
+    return dwarfs::safe_main([&] 
+        { 
+            char* _argv[3];
+            _argv[0] = argv[0];
+            _argv[1] = "fs0.bin";
+            _argv[2] = "/home/tebako";
+            int ret = dwarfs::run_dwarfs(3, _argv);
+            while(!ret) { }
+            return ret;
+        });
 }
