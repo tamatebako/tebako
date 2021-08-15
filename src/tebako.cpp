8db93cccebb641bb1a4da37a58316ba6757e8285
Maxim Samsonov <m.samsonov@computer.org>
2021-08-16 00:26:57 +0300

    A program that executes a script from inside of DwarFS

--- src/tebako.cpp
+++ src/tebako.cpp
@@ -83,7 +83,8 @@ int main(int argc, char** argv) {
             }
 
             if (ret == 0) {
-                std::string cmd(tebako::fs_mount_point);
+                std::string cmd("sudo ");
+                cmd += tebako::fs_mount_point;
                 cmd += "/test-0.sh";
                 system(cmd.c_str());
                 dwarfs::stop_fuse_session();
