78a3d784a4d74c038f714a19afc373bbea273ee0
Maxim Samsonov <m.samsonov@computer.org>
2021-09-24 11:13:31 +0300

    Added errexit support

--- tests/scripts/press_with_failure.sh
+++ tests/scripts/press_with_failure.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright (c) 2021, [Ribose Inc](https://www.ribose.com).
 # All rights reserved.
@@ -31,15 +31,28 @@
 # $1  tebako press --root=$1
 # $2  tebako press --entry-point=$2
 # $3  expected exit code (103 or 104)
+# returns 0 - tebako press failed with expeced return code [Test OK]
+#         1 - tebako press succeeded [Test failed]
+#         2 - tebako press failed with unexpected return code [Test failed]
 
-testTebakoPressFailure() {
-  $DIR/tebako press --root=$1 --entry-point=$2
-  assertEquals $3 $?
-}
-
-# .......................................
 DIR0="$( cd "$( dirname "$0" )" && pwd )"
 DIR="$( cd $DIR0/../../bin && pwd )"
-echo Expecting "$DIR/tebako press --root=$1 --entry=point=$2 to fail with exit code $3" 
-. $DIR0/../shunit2/shunit2
+echo Expecting "$DIR/tebako press --root=$1 --entry-point=$2 to fail with exit code $3" 
+
+$DIR/tebako press "--root=$1" --entry-point=$2
+
+res=$?
+
+if  [ $res -eq 0 ] ; then 
+  echo "'tebako press' has succeeded unexpectedly [Test failed]"
+  exit 1 
+else 
+  if [ $res -eq $3 ] ; then 
+    echo "'tebako press' failed as expected with code $3 [Test OK]"
+    exit 0
+  else
+    echo "'tebako press' failed with code $res but $3 was expected [Test failed]"
+    exit 2
+  fi
+fi
 
