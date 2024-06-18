830c9da630eb7175595faf5932269d107ede00b6
Maxim Samsonov <m.samsonov@computer.org>
2024-06-18 22:18:12 +0300

    Fixed setup-tebako minor issues (cache versioning, setup warning on MacOs runners)

--- tests/test-01/tebako-test-run.rb
+++ tests/test-01/tebako-test-run.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 puts "Hello!  This is test-01 talking from inside DwarFS"
-# puts $LOAD_PATH
 
 puts "Gem path: #{Gem.path}"
 puts "Rubygems version: #{Gem::rubygems_version}"
