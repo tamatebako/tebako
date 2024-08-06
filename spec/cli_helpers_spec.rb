6ae767c6310ff004bb1c41ddb41ece2093ae69bf
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2024-08-06 20:10:56 +0300

    test: more cli tests

--- spec/cli_helpers_spec.rb
+++ spec/cli_helpers_spec.rb
@@ -121,4 +121,94 @@ RSpec.describe Tebako::CliHelpers do # rubocop:disable Metrics/BlockLength
       end
     end
   end
+
+  describe "#version_key" do
+    it "returns the correct version key" do
+      allow(self).to receive(:source).and_return("/path/to/source")
+      expect(version_key).to eq("#{Tebako::VERSION} at /path/to/source")
+    end
+  end
+
+  describe "#version_cache" do
+    it "returns the correct version and source from the cache file" do
+      version_file_content = "#{Tebako::VERSION} at /path/to/source\n"
+      allow(self).to receive(:deps).and_return("/path/to/deps")
+      allow(File).to receive(:join).with("/path/to/deps", Tebako::E_VERSION_FILE).and_return("/path/to/version_file")
+      allow(File).to receive(:open).with("/path/to/version_file").and_yield(StringIO.new(version_file_content))
+    end
+  end
+
+  describe "#version_cache_check" do # rubocop:disable Metrics/BlockLength
+    before do
+      allow(self).to receive(:version_cache).and_return(match_data)
+      allow(self).to receive(:version_unknown)
+      allow(self).to receive(:version_mismatch)
+      allow(self).to receive(:version_source_mismatch)
+    end
+
+    context "when version cache is unknown" do
+      let(:match_data) { nil }
+
+      it "calls version_unknown" do
+        expect(self).to receive(:version_unknown)
+        version_cache_check
+      end
+    end
+
+    context "when version does not match" do
+      let(:match_data) { { version: "0.7.0", source: "/path/to/source" } }
+      it "calls version_mismatch" do
+        expect(self).to receive(:version_mismatch).with("0.7.0")
+        version_cache_check
+      end
+    end
+
+    context "when source does not match" do
+      let(:match_data) { { version: Tebako::VERSION, source: "/different/source" } }
+
+      it "calls version_source_mismatch" do
+        allow(self).to receive(:source).and_return("/path/to/source")
+        expect(self).to receive(:version_source_mismatch).with("/different/source")
+        version_cache_check
+      end
+    end
+
+    context "when version and source match" do
+      let(:match_data) { { version: Tebako::VERSION, source: "/path/to/source" } }
+
+      it "does not call any mismatch methods" do
+        allow(self).to receive(:source).and_return("/path/to/source")
+        expect(self).not_to receive(:version_unknown)
+        expect(self).not_to receive(:version_mismatch)
+        expect(self).not_to receive(:version_source_mismatch)
+        version_cache_check
+      end
+    end
+  end
+  describe "#version_mismatch" do
+    it "prints the correct message and cleans the cache" do
+      expect(self).to receive(:puts)
+        .with("Tebako cache was created by a gem version 0.9.0 and cannot be used for gem version #{Tebako::VERSION}")
+      expect(self).to receive(:clean_cache)
+      allow(Tebako).to receive(:VERSION).and_return("1.0.0")
+      version_mismatch("0.9.0")
+    end
+  end
+  describe "#version_source_mismatch" do
+    it "prints the correct message and cleans the output" do
+      expect(self).to receive(:puts)
+        .with("CMake cache was created for a different source directory '/different/source' " \
+              "and cannot be used for '/path/to/source'")
+      expect(self).to receive(:clean_output)
+      allow(self).to receive(:source).and_return("/path/to/source")
+      version_source_mismatch("/different/source")
+    end
+  end
+  describe "#version_unknown" do
+    it "prints the correct message and cleans the cache" do
+      expect(self).to receive(:puts).with("CMake cache version was not recognized, cleaning up")
+      expect(self).to receive(:clean_cache)
+      version_unknown
+    end
+  end
 end
