febacbe29b20e831b663f8e7f23eb015a4fe44eb
Maxim Samsonov <m.samsonov@computer.org>
2024-08-27 12:12:20 +0300

    test: move more tests from shunit to rspec, cover new Ruby build configuration

--- spec/deploy_helper_spec.rb
+++ spec/deploy_helper_spec.rb
@@ -26,8 +26,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 require_relative "../lib/tebako/deploy_helper"
+# rubocop:disable Metrics/BlockLength
 
-RSpec.describe Tebako::DeployHelper do # rubocop:disable Metrics/BlockLength
+RSpec.describe Tebako::DeployHelper do
   let(:fs_root) { "/fs/root" }
   let(:fs_entrance) { "/fs/entrance" }
   let(:fs_mount_point) { "/fs/mount_point" }
@@ -77,6 +78,7 @@ RSpec.describe Tebako::DeployHelper do # rubocop:disable Metrics/BlockLength
       expect(deploy_helper).to have_received(:configure_commands)
     end
   end
+
   describe "#check_cwd" do
     context "when tebako cwd setting is nil" do
       it "returns (check succeeds)" do
@@ -101,4 +103,204 @@ RSpec.describe Tebako::DeployHelper do # rubocop:disable Metrics/BlockLength
       end
     end
   end
+
+  describe "#configure_commands" do
+    context "when os_type is msys" do
+      before do
+        deploy_helper.instance_variable_set(:@os_type, "msys")
+        deploy_helper.instance_variable_set(:@tbd, "/path/to/tbd")
+        deploy_helper.send(:configure_commands)
+      end
+
+      it "sets the correct command suffixes" do
+        expect(deploy_helper.instance_variable_get(:@cmd_suffix)).to eq(".cmd")
+        expect(deploy_helper.instance_variable_get(:@bat_suffix)).to eq(".bat")
+      end
+
+      it "sets the correct gem and bundler commands" do
+        expect(deploy_helper.instance_variable_get(:@gem_command)).to eq("/path/to/tbd/gem.cmd")
+        expect(deploy_helper.instance_variable_get(:@bundler_command)).to eq("/path/to/tbd/bundle.bat")
+      end
+
+      it "sets the correct force ruby platform" do
+        expect(deploy_helper.instance_variable_get(:@force_ruby_platform)).to eq("true")
+      end
+
+      it "sets the correct nokogiri option" do
+        expect(deploy_helper.instance_variable_get(:@nokogiri_option)).to eq("--use-system-libraries")
+      end
+    end
+
+    context "when os_type is not msys" do
+      before do
+        deploy_helper.instance_variable_set(:@os_type, "linux")
+        deploy_helper.instance_variable_set(:@tbd, "/path/to/tbd")
+        deploy_helper.send(:configure_commands)
+      end
+
+      it "sets the correct command suffixes" do
+        expect(deploy_helper.instance_variable_get(:@cmd_suffix)).to eq("")
+        expect(deploy_helper.instance_variable_get(:@bat_suffix)).to eq("")
+      end
+
+      it "sets the correct gem and bundler commands" do
+        expect(deploy_helper.instance_variable_get(:@gem_command)).to eq("/path/to/tbd/gem")
+        expect(deploy_helper.instance_variable_get(:@bundler_command)).to eq("/path/to/tbd/bundle")
+      end
+
+      it "sets the correct force ruby platform" do
+        expect(deploy_helper.instance_variable_get(:@force_ruby_platform)).to eq("false")
+      end
+
+      it "sets the correct nokogiri option" do
+        expect(deploy_helper.instance_variable_get(:@nokogiri_option)).to eq("--no-use-system-libraries")
+      end
+    end
+  end
+
+  describe "#configure_scenario" do
+    context "when @gs_length is 0" do
+      before do
+        deploy_helper.instance_variable_set(:@gs_length, 0)
+      end
+
+      it "calls configure_scenario_no_gemspec" do
+        expect(deploy_helper).to receive(:configure_scenario_no_gemspec)
+        deploy_helper.send(:configure_scenario)
+      end
+    end
+
+    context "when @gs_length is 1" do
+      before do
+        deploy_helper.instance_variable_set(:@gs_length, 1)
+      end
+
+      context "and @gf_length is positive" do
+        before do
+          deploy_helper.instance_variable_set(:@gf_length, 1)
+          deploy_helper.send(:configure_scenario)
+        end
+
+        it "sets @scenario to :gemspec_and_gemfile" do
+          expect(deploy_helper.instance_variable_get(:@scenario)).to eq(:gemspec_and_gemfile)
+        end
+      end
+
+      context "and @gf_length is 0" do
+        before do
+          deploy_helper.instance_variable_set(:@gf_length, 0)
+          deploy_helper.send(:configure_scenario)
+        end
+
+        it "sets @scenario to :gemspec" do
+          expect(deploy_helper.instance_variable_get(:@scenario)).to eq(:gemspec)
+        end
+      end
+    end
+
+    context "when @gs_length is greater than 1" do
+      before do
+        deploy_helper.instance_variable_set(:@gs_length, 2)
+      end
+
+      it "raises a Tebako::Error" do
+        expect do
+          deploy_helper.send(:configure_scenario)
+        end.to raise_error(Tebako::Error,
+                           "Multiple Ruby gemspecs found in #{deploy_helper.instance_variable_get(:@fs_root)}")
+      end
+    end
+  end
+
+  describe "#configure_scenario_no_gemspec" do
+    context "when @gf_length is positive" do
+      before do
+        deploy_helper.instance_variable_set(:@gf_length, 1)
+        deploy_helper.instance_variable_set(:@g_length, 0)
+        deploy_helper.send(:configure_scenario_no_gemspec)
+      end
+
+      it "sets @scenario to :gemfile" do
+        expect(deploy_helper.instance_variable_get(:@scenario)).to eq(:gemfile)
+      end
+    end
+
+    context "when @gf_length is 0 and @g_length is positive" do
+      before do
+        deploy_helper.instance_variable_set(:@gf_length, 0)
+        deploy_helper.instance_variable_set(:@g_length, 1)
+        deploy_helper.send(:configure_scenario_no_gemspec)
+      end
+
+      it "sets @scenario to :gem" do
+        expect(deploy_helper.instance_variable_get(:@scenario)).to eq(:gem)
+      end
+    end
+
+    context "when both @gf_length and @g_length are 0" do
+      before do
+        deploy_helper.instance_variable_set(:@gf_length, 0)
+        deploy_helper.instance_variable_set(:@g_length, 0)
+        deploy_helper.send(:configure_scenario_no_gemspec)
+      end
+
+      it "sets @scenario to :simple_script" do
+        expect(deploy_helper.instance_variable_get(:@scenario)).to eq(:simple_script)
+      end
+    end
+  end
+  describe "#needs_bundler?" do
+    context "when @gf_length is greater than 0" do
+      before do
+        deploy_helper.instance_variable_set(:@gf_length, 1)
+      end
+
+      context "and @ruby_ver is less than 3.1" do
+        before do
+          deploy_helper.instance_variable_set(:@ruby_ver, Tebako::RubyVersion.new("3.0.7"))
+        end
+
+        it "returns true" do
+          expect(deploy_helper.needs_bundler?).to be true
+        end
+      end
+
+      context "and @ruby_ver is 3.1 or greater" do
+        before do
+          deploy_helper.instance_variable_set(:@ruby_ver, Tebako::RubyVersion.new("3.1.6"))
+        end
+
+        it "returns false" do
+          expect(deploy_helper.needs_bundler?).to be false
+        end
+      end
+    end
+
+    context "when @gf_length is 0" do
+      before do
+        deploy_helper.instance_variable_set(:@gf_length, 0)
+      end
+
+      context "and @ruby_ver is less than 3.1" do
+        before do
+          deploy_helper.instance_variable_set(:@ruby_ver, Tebako::RubyVersion.new("3.0.7"))
+        end
+
+        it "returns false" do
+          expect(deploy_helper.needs_bundler?).to be false
+        end
+      end
+
+      context "and @ruby_ver is 3.1 or greater" do
+        before do
+          deploy_helper.instance_variable_set(:@ruby_ver, Tebako::RubyVersion.new("3.1.6"))
+        end
+
+        it "returns false" do
+          expect(deploy_helper.needs_bundler?).to be false
+        end
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
